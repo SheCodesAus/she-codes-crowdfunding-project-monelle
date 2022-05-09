c6f4db5854ae65af6f467c8c1c97f1d128794435
Monelle Trachez <monelle.trachez@gmail.com>
2022-05-09 21:32:11 +0800

    Add Logout function

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 // Styles
 import "./Nav.css";
 
 
 function Nav() {
+
+    const navigate = useNavigate();
+
+    const navigateToLogin = () => {
+        navigate("/login")
+    }
+
+    const handleSignOut = () => {
+        window.localStorage.removeItem("token")
+        navigateToLogin()
+    }
+
+    const checkUser = () => {
+        const isUserLoggedIn = !!window.localStorage.getItem("token");
+        // console.log("isUserLoggedIn", isUserLoggedIn)
+
+        return isUserLoggedIn
+            ? <button onClick={handleSignOut} className="button">Log out</button>
+            : <button onClick={navigateToLogin} className="button">Login</button>
+    }
+
+
     return (
         <section className="navbar">
             <nav className="left-menu">
@@ -14,9 +36,11 @@ function Nav() {
             </nav>
             <nav className="right-menu">
             <Link className="button" to="/search/">Search </Link>
-            <Link className="button" to="/login/">Log in </Link>
+            {/* <Link className="button" to="/login/">Log in </Link> */}
+            {checkUser()}
             </nav>
         </section>
+
     );
 }
 
