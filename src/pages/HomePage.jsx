a619e5a4794925bdc4c58d867565f954cb7aaf71
Monelle Trachez <monelle.trachez@gmail.com>
2022-05-10 15:26:58 +0800

    Building the HomePage with components - unfinished

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 // Components
 import ProjectCard from '../components/ProjectCard/ProjectCard';
+
+// Style
+import "./HomePage.css"
+// import girl from "./blue-girl-toa-heftiba-unsplash"
 
 
 function HomePage() {
@@ -21,6 +26,24 @@ function HomePage() {
 }, []);
 
     return (
+    <main>
+    <section className="introduction">
+        <article className="left-section">
+            <h1>Let them be Huge</h1>   
+            <p>Huge is a crowdfunding platform designed  to connect visual artists to their
+            fan base beyond the ‘classic’ channels.<br />
+            As mainstream channels get crowded (social media overload anyone?) or inaccessible
+            (hello lockdowns and restrictions), we provide a new model of community building
+            and meaningful engagement through art.<br /><br />
+            </p>
+        </article>
+        <article className="right-section">
+            <Link to="/projects" className="action-button">Start browsing projects</Link>
+            {/* <img className="intro-illustration" src={ girl }></img> */}
+        </article>
+    </section>
+
+
     <div id="project-list">
         {projectList.map((project, key) => {
             return (
@@ -30,6 +53,7 @@ function HomePage() {
             );
         })}
     </div>
+    </main>
     );
 }
 
